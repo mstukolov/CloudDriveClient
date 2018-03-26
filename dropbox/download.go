c5cd6766118091769ee967c6f27ef462354e14b0
mstukolov <maxim.stukolov@gmail.com>
2018-03-26 16:46:54 +0300

    26.03.2018 add dropbox download

--- dropbox/download.go
+++ dropbox/download.go
@@ -17,20 +17,19 @@ func Download(login string, password string, url string) (err error){
 	println("This Dropbox download utility")
 	fmt.Printf("login: %s, password: %s, url: %s\n", login, password, url)
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter URL: ")
-	URI, _ := reader.ReadString('\n')
+	fmt.Print("Enter fileID: ")
+	fileID, _ := reader.ReadString('\n')
 	fmt.Print("Enter file name: ")
 	fileName, _ := reader.ReadString('\n')
 
-	fmt.Println(URI)
-	//s := []string{"https://www.dropbox.com/s/", strings.TrimSuffix(fileID, "\n"), "/", strings.TrimSuffix(fileName, "\n"), "?dl=1"}
+	s := []string{"https://www.dropbox.com/s/", strings.TrimSuffix(fileID, "\n"), "/", strings.TrimSuffix(fileName, "\n"), "?dl=1"}
 	//s := []string{"https://www.dropbox.com/s/", "1uohkqwl2k2d1du/anyconnect.rar", "?dl=1"}
 	//s := []string{"https://www.dropbox.com/s/", strings.TrimSuffix(fileID, "\n"), "?dl=1"}
-	/*s := []string{"this", strings.TrimSuffix(fileID, "\n"), "is", "joined", "string\n"}
-		URI := strings.Join(s, "")*/
 
-	//Run(URI, strings.TrimSuffix(fileName, "\n"))
-	return Run("https://www.dropbox.com/s/1uohkqwl2k2d1du/anyconnect.rar?dl=1", strings.TrimSuffix(fileName, "\n"))
+	URI := strings.Join(s, "")
+
+	return Run(URI, strings.TrimSuffix(fileName, "\n"))
+	//return Run("https://www.dropbox.com/s/1uohkqwl2k2d1du/anyconnect.rar?dl=1", strings.TrimSuffix(fileName, "\n"))
 	//return nil
 }
 
